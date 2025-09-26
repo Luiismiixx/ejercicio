0637d1ea5a15e55e870db1369e93245a7c919b9c
Luiismiixx <luismiazy@gmail.com>
2025-09-26 12:55:03 -0500

    version4

--- src/main/java/es/etg/dax/model/Alumno.java
+++ src/main/java/es/etg/dax/model/Alumno.java
@@ -6,5 +6,7 @@ import lombok.Data;
 
 @Data @AllArgsConstructor
 public class Alumno {
-    
+    private String nombre;
+    private String apellidos;
+    private int edad;
 }
---
+++ src/main/java/es/etg/dax/model/database/AlumnoDAO.java
@@ -0,0 +1,5 @@
+package es.etg.dax.model.database;
+
+public class AlumnoDAO {
+    
+}
